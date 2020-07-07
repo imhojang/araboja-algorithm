ce0e5467ed174d15a43db6615d1c2fa6322caa53
oyvvl <940709@gmail.com>
2020-07-07 20:33:00 +0900

    add solution #2 - anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,8 +8,9 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-
 // solution #1
+
+/*
 
 function anagrams(stringA, stringB) {
 	function generateCharMap(string) {
@@ -39,6 +40,39 @@ function anagrams(stringA, stringB) {
 }
 
 anagrams('One one', 'One one c');
+
+*/
+
+// solution #2
+
+function anagrams(stringA, stringB) {
+	const charMapA = buildCharMap(stringA);
+	const charMapB = buildCharMap(stringB);
+
+	if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+		return false;
+	}
+
+	for (let char in charMapA) {
+		if (charMapA[char] !== charMapB[char]) {
+			return false;
+		}
+	}
+
+	return true;
+
+	function buildCharMap(string) {
+		const charMap = {};
+
+		const newString = string.replace(/[^\w]/g, '').toLowerCase();
+
+		for (let char of newString) {
+			charMap[char] = charMap[char] + 1 || 1;
+		}
+
+		return charMap;
+	}
+}
 
 // word.replace(/[^\w]/g, '').toLowerCase()
 module.exports = anagrams;
