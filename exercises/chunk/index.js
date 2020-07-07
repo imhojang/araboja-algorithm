cef3e32120d5f536e8fbe734064f80ec4b28c90e
oyvvl <940709@gmail.com>
2020-07-07 18:33:53 +0900

    add solution #3 - chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,26 +9,29 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 // solution #1
-// function chunk(array, size) {
-// 	let chunks = [];
-// 	let collection = [];
+/*
+function chunk(array, size) {
+	let chunks = [];
+	let collection = [];
 
-// 	for (let i = 0; i < array.length; i++) {
-// 		chunks.push(array[i]);
+	for (let i = 0; i < array.length; i++) {
+		chunks.push(array[i]);
 
-// 		if ((i + 1) % size === 0) {
-// 			collection.push(chunks);
-// 			chunks = [];
-// 		} else if (i === array.length - 1) {
-// 			collection.push(chunks);
-// 		}
-// 	}
+		if ((i + 1) % size === 0) {
+			collection.push(chunks);
+			chunks = [];
+		} else if (i === array.length - 1) {
+			collection.push(chunks);
+		}
+	}
 
-// 	return collection;
-// }
+	return collection;
+}
+*/
 
 // solution #2
 
+/*
 function chunk(array, size) {
 	const chunked = [];
 
@@ -44,9 +47,18 @@ function chunk(array, size) {
 
 	return chunked;
 }
+*/
 
+// solution #3 - array.slice
 
-
+function chunk(array, size) {
+	let chunked = [];
+	for (let i = 0; i < array.length; i += size) {
+		const slice = array.slice(i, i + size);
+		chunked.push(slice);
+	}
+	return chunked;
+}
 
 // chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
 
