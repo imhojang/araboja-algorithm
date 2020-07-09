0b4358dbc7aed6bbfb992aefb50930c532bd2770
oyvvl <940709@gmail.com>
2020-07-10 00:14:11 +0900

    add solution - recursive by me

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -16,22 +16,41 @@
 
 // solution #1 - iterative solution
 
-function pyramid(n) {
-  for (let row = 0; row < n; row++) {
-    let str = '';
-    let maxCol = n * 2 - 1;
-    let sideSpace = n - row - 1;
-    for (let col = 0; col < maxCol; col++) {
-      if (col < sideSpace || col >= maxCol - sideSpace) {
-        str += ' ';
-      } else {
-        str += '#';
-      }
-    }
-    console.log(str);
-  }
+// function pyramid(n) {
+//   for (let row = 0; row < n; row++) {
+//     let str = '';
+//     let maxCol = n * 2 - 1;
+//     let sideSpace = n - row - 1;
+//     for (let col = 0; col < maxCol; col++) {
+//       if (col < sideSpace || col >= maxCol - sideSpace) {
+//         str += ' ';
+//       } else {
+//         str += '#';
+//       }
+//     }
+//     console.log(str);
+//   }
+// }
+
+// solution #2 recursive solution
+
+function pyramid(n, row = 0, str = '') {
+	if (n === row) {
+		return;
+	}
+
+	let maxCol = n * 2 - 1;
+	if (str.length === maxCol) {
+		console.log(str);
+		return pyramid(n, row + 1);
+	}
+
+	let sideSpace = n - row - 1;
+	if (str.length < sideSpace || str.length >= maxCol - sideSpace) {
+		pyramid(n, row, str + ' ');
+	} else {
+		pyramid(n, row, str + '#');
+	}
 }
-
-
 
 module.exports = pyramid;
