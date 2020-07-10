39ff52f2173ba2444be9c1a4c74a8c4b86231ba2
oyvvl <940709@gmail.com>
2020-07-10 15:40:26 +0900

    add solution #1 - iterative with hashmap

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,20 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// solution #1 - iterative with hashmap
+
+function vowels(str) {
+	const vowels = { a: 1, e: 1, i: 1, o: 1, u: 1 };
+
+	let count = 0;
+
+	for (let char of str.toLowerCase()) {
+		if (vowels[char]) {
+			count++;
+		}
+	}
+
+	return count;
+}
 
 module.exports = vowels;
