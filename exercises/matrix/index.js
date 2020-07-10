a8d083d1080ee36faeb4dab02fb5985ffa474ce2
oyvvl <940709@gmail.com>
2020-07-10 19:15:27 +0900

    add solution #1

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,92 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+// solution #1 
+function matrix(n) {
+	let results = [];
+
+	for (let i = 0; i < n; i++) {
+		results.push(Array(n));
+	}
+
+	let startRow = 0;
+	let endRow = n - 1;
+	let startCol = 0;
+	let endCol = n - 1;
+
+	let counter = 1;
+	let i;
+	while (startRow <= endRow && startCol <= endCol) {
+		i = startCol;
+		while (i <= endCol) {
+			results[startRow][i] = counter;
+			counter++;
+			i++;
+		}
+
+		startRow++;
+
+		i = startRow;
+		while (i <= endRow) {
+			results[i][endCol] = counter;
+			counter++;
+			i++;
+		}
+
+		endCol--;
+
+		i = endCol;
+		while (i >= startCol) {
+			results[endRow][i] = counter;
+			counter++;
+			i--;
+		}
+
+		endRow--;
+
+		i = endRow;
+		while (i >= startRow) {
+			results[i][startCol] = counter;
+			counter++;
+			i--;
+		}
+
+		startCol++;
+	}
+
+	return results;
+}
+
+/*
+    CREATE counter variable  
+
+    AS LONG AS start_row is less than end_row AND 
+    start_col is less than to end_col
+      
+      while i !== endCol
+        - fill in [startRow][i]
+        - increment count
+      
+      INCREMENT startRow
+
+      while i !== endRow
+        - fill in [i][endCol]
+        - increment count
+
+      DECREMENT endCol
+
+      while i !== startCol
+        - fill in [endRow][i]
+        - increment count
+
+      DECREMENT endRow
+      
+       while i !== startRow
+        - fill in [i][startCol]
+        - incrementCount
+
+      INCREMENT startCol
+}
+*/
 
 module.exports = matrix;
